a173ffdccf715d41f4f342c574c0801191c7e09e
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2015-08-14 17:26:47 -0400

    configure access facet, capitalize relator terms for author

--- spec/search/search_spec.rb
+++ spec/search/search_spec.rb
@@ -4,14 +4,14 @@ unless ENV['TRAVIS']
     it 'The novel is first in a keyword search' do
       jane_eyre_novel_id = "1317967"
       resp = solr_resp_doc_ids_only({'q'=>'Jane Eyre'})
-      resp.should include(jane_eyre_novel_id).as_first_result
+      expect(resp).to include(jane_eyre_novel_id).as_first_result
     end
 
     it 'Appears before book about Jane Eyre' do
       book_about_id = "323739"
       jane_eyre_novel_id = "1317967"
       resp = solr_resp_doc_ids_only({'q'=>'Jane Eyre'})
-      resp.should include(jane_eyre_novel_id).before(book_about_id)
+      expect(resp).to include(jane_eyre_novel_id).before(book_about_id)
     end
   end
 end
